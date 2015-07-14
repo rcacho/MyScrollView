7de72135e7e899f99b50ed0f526f9e425ab7a42c
rcacho <rcacho@hotmail.ca>
2015-07-14 09:42:54 -0700

    init

---
+++ MyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyScrollView
+//
+//  Created by ricardo antonio cacho on 2015-07-14.
+//  Copyright (c) 2015 ricardo antonio cacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyScrollView
+//
+//  Created by ricardo antonio cacho on 2015-07-14.
+//  Copyright (c) 2015 ricardo antonio cacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
